acda326c4c52ac5bc758024da274b9c88d5ff988
RRP12 <rushikeshpatil8208@gmail.com>
2024-01-29 23:45:41 +0530

    fixed the build error

--- components/features/inventory/inventory.js
+++ components/features/inventory/inventory.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { calculatePrice, getCurrencySymbol } from "../../../utilities/utilities"
 import { addItem } from "../cart/cartSlice.js"
 import { loadData } from "./inventorySlice"
-
+//added image component 
 export const Inventory = ({ inventory, currencyFilter, dispatch }) => {
   const onMount = () => {
     dispatch(loadData())
